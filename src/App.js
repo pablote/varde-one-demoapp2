86b41534ff2b52839505101bc1d4251de48b7752
Pablo Compagni <pablocompagni@gmail.com>
2023-03-06 18:04:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <div>change 1</div>
         <div>change 2</div>
         <div>change 3</div>
+        <div>change 4 from upstream</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
